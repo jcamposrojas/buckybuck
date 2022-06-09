613f6b632b62b398a1a5ef8b5ce449a7f6c8131e
Juan I. Campos R <jicampos@devmobile.cl>
2022-06-09 14:18:05 -0400

    Nomina CL v2

--- l10n_cl_hr/model/hr_payslip.py
+++ l10n_cl_hr/model/hr_payslip.py
@@ -5,6 +5,8 @@ from odoo import api, fields, models, tools, _
 from odoo.addons import decimal_precision as dp
 from odoo.exceptions import UserError, ValidationError
 
+import logging
+_logger = logging.getLogger(__name__)
 
 class HrPayslip(models.Model):
     _inherit = 'hr.payslip'
@@ -38,14 +40,22 @@ class HrPayslip(models.Model):
         return super(HrPayslip, self).create(vals)
 
     @api.model
-    def get_worked_day_lines(self, contracts, date_from, date_to):
-        res = super(HrPayslip, self).get_worked_day_lines(contracts, date_from, date_to)
+    def _get_worked_day_lines(self, domain=None, check_out_of_contract=True):
+    #def get_worked_day_lines(self, contracts, date_from, date_to):
+        res = super(HrPayslip, self)._get_worked_day_lines(domain, check_out_of_contract)
         temp = 0 
         dias = 0
         attendances = {}
         leaves = []
+
         for line in res:
-            if line.get('code') == 'WORK100':
+            entry = self.env['hr.work.entry.type'].search(
+                [
+                    ('id', '=', line.get('work_entry_type_id')),
+                ])
+
+            if entry.code == 'WORK100':
+            #if line.get('work_entry_type_id.code') == 'WORK100':
                 attendances = line
             else:
                 leaves.append(line)
@@ -53,15 +63,22 @@ class HrPayslip(models.Model):
             temp += leave.get('number_of_days') or 0
         #Dias laborados reales para calcular la semana corrida
         effective = attendances.copy()
+        entry_effective = self.env['hr.work.entry.type'].search(
+                [
+                    ('code', '=', 'EFF100'),
+                ])
         effective.update({
             'name': _("Dias de trabajo efectivos"),
             'sequence': 2,
-            'code': 'EFF100',
+            #'code': 'EFF100',
+            'work_entry_type_id': entry_effective.id,
         })
         # En el caso de que se trabajen menos de 5 días tomaremos los dias trabajados en los demás casos 30 días - las faltas
         # Estos casos siempre se podrán modificar manualmente directamente en la nomina.
         # Originalmente este dato se toma dependiendo de los dias del mes y no de 30 dias
         # TODO debemos saltar las vacaciones, es decir, las vacaciones no descuentan dias de trabajo. 
+        _logger.info('NUMBEROFDAY')
+        _logger.info(effective.get('number_of_days'))
         if (effective.get('number_of_days') or 0) < 5:
             dias = effective.get('number_of_days')
         else:
